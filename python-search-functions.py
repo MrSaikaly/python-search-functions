5f9e7ee5c9ffe7b612465bf53def0044e5bc89be
Jad Saikaly <saikalyss@gmail.com>
2025-06-20 10:46:24 +0300

    feat: added more advanced binary search functions

--- python-search-functions.py
+++ python-search-functions.py
@@ -32,12 +32,56 @@ def binary_search(value_list,target):
 
     return -1
 
-    print()
+#binary search that return all results
 
+def binary_search_first_index(value_list,target):
+    #creating the binary search
 
+    low_index=0 #setting low index
+    high_index=len(value_list)-1 # setting high index
+    first=-1
+
+    while(low_index<=high_index): #while loop
+        mid =(low_index + high_index) //2 #setting middle index 
+
+        if value_list[mid] == target : # we found our index
+            first= mid
+            high_index=mid-1
+
+        elif value_list[mid] > target: # if list value on index mid bigger than target move the high index 
+            high_index= mid-1
+        else:# else list value on index mid smaller than target move the low index 
+            low_index=mid+1
+
+    return first
+
+def binary_search_last_index(value_list,target):
+    #creating the binary search
+
+    low_index=0 #setting low index
+    high_index=len(value_list)-1 # setting high index
+    last=-1
+
+    while(low_index<=high_index): #while loop
+        
+        mid =(low_index + high_index) //2 #setting middle index 
+
+        if value_list[mid] == target : # we found our index
+            last= mid
+            low_index=mid+1
+
+        elif value_list[mid] > target: # if list value on index mid bigger than target move the high index 
+            high_index= mid-1
+        else:# else list value on index mid smaller than target move the low index 
+            low_index=mid+1
+
+    return last
 #using the function
 
-my_list=[10,20,30,45,50,70]
+my_list=[100,20,30, 10,10,10 ,45,50,70]
+my_list.sort()
+print(my_list)
 
-print("your target value:", 50, " is on index :_", linear_search(my_list,51000))
-print("your target value:", 50, " is on index :_", binary_search(my_list,10))
+print("your target value:", 50, " is on index : ", linear_search(my_list,50))
+print("your target value:", 10, " is on index : ", binary_search_first_index(my_list,10))
+print("your target value:", 10, " is on index : ", binary_search_last_index(my_list,10))
